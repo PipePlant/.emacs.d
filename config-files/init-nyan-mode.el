a6cb04edadb2f6edcdee425c3fa3b70cfdb4a024
lingchen <qopclc@gmail.com>
2016-04-19 16:57:57 +0800

    add nyan

---
+++ config-files/init-nyan-mode.el
@@ -0,0 +1,6 @@
+'(:eval (when nyan-mode (list (nyan-create))))
+(nyan-mode t)
+; (nyan-start-animation);;开始舞动吧（会耗cpu资源）  
+
+
+(provide 'init-nyan-mode)
--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path
 	(expand-file-name "~/.emacs.d/config-files"))
 ; (require 'init-benchmarking)
+'(:eval (when nyan-mode (list (nyan-create))))
 
 ;; ````````` common config `````````
 (require 'init-common-config)
@@ -10,3 +11,18 @@
 (require 'init-yasnippet)
 ;; ````````` company `````````
 (require 'init-company)
+;; ````````` nyan-mode `````````
+(require 'init-nyan-mode)
+;; ````````` auto-save `````````
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-time-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
