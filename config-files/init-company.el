624bac1c007d24d971eb7bb47d0aea4c706acf8d
lingchen <qopclc@gmail.com>
2016-04-20 11:39:32 +0800

    update config file

--- config-files/init-company.el
+++ config-files/init-company.el
@@ -1,6 +1,5 @@
-(add-to-list 'load-path 
-	"~./.emacs.d/elpa/company-20160413.1347")
 (autoload 'company-mode "company" nil t)
+
 (setq company-idle-delay 0)
 (add-hook 'after-init-hook 'global-company-mode)
 
